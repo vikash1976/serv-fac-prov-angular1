85a9faf746e56edeb46d608aafb0deedcb9c22b6
vikash1976 <vikash1976@gmail.com>
2016-01-25 12:20:04 +0530

    Update srv-fac-prv.js

--- srv-fac-prv.js
+++ srv-fac-prv.js
@@ -69,10 +69,3 @@ srvFacPrvApp.controller('mainPrvCtrl', ['$scope', 'nameProvider', function($scop
 }])
 
 
-/*
-When you’re using a Factory you create an object, add properties to it, then return that same object. When you pass this service into your controller, those properties on the object will now be available in that controller through your factory.
-
-When you’re using Service, it’s instantiated with the ‘new’ keyword. Because of that, you’ll add properties to ‘this’ and the service will return ‘this’. When you pass the service into your controller, those properties on ‘this’ will now be available on that controller through your service.
-
-Providers are the only service you can pass into your .config() function. Use a provider when you want to provide module-wide configuration for your service object before making it available.
-*/
